ffc347ef5b35916d0131303ad6376baf2163227d
omokun <omokun1215@gmail.com>
2021-07-14 21:16:30 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   # get "/", to: "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update,]
 end
