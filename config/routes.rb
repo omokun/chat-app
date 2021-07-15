413530ada60be9b2e3d1c285b5aa92f4b9ab807c
omokun <omokun1215@gmail.com>
2021-07-15 18:44:17 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   # get "/", to: "messages#index"
-  resources :users, only: [:edit, :update,]
-  resources :rooms, only: [:new, :create]
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
